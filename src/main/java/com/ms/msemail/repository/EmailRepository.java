aa6c54130b300ac32e7a95e46a617793551ee07c
Elaine Molina <ElaineMolina>
2024-03-20 12:45:21 -0300

    configurando smpt google para envio de email, método sendEmail

--- src/main/java/com/ms/msemail/repository/EmailRepository.java
+++ src/main/java/com/ms/msemail/repository/EmailRepository.java
@@ -1,9 +1,7 @@
 package com.ms.msemail.repository;
 
-import com.ms.msemail.dtos.EmailDto;
+import com.ms.msemail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface EmailRepository extends JpaRepository<EmailDto, UUID> {
+public interface EmailRepository extends JpaRepository<EmailModel, Long> {
 }
