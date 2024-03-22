4767112d8cf4b62938bc4eba092fba5aeee1a76f
Elaine Molina <ElaineMolina>
2024-03-22 16:12:12 -0300

    configuração da conexão rabbitmq

--- src/main/java/com/ms/msemail/repository/EmailRepository.java
+++ src/main/java/com/ms/msemail/repository/EmailRepository.java
@@ -3,5 +3,7 @@ package com.ms.msemail.repository;
 import com.ms.msemail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
